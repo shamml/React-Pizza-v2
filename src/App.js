666a58806748310c41d260df6edd2e9a9081bd89
sham <shamil.gachaev@gmail.com>
2022-10-19 23:25:24 +0300

    Скелетон при загрузке пицц

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import { Header } from "./components/Header";
 import { Categories } from "./components/Categories";
 import { Sort } from "./components/Sort/Sort";
 import { PizzaBlock } from "./components/PizzaBlock";
+import { Skeleton } from "./components/PizzaBlock/Skeleton";
 
 export function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+  const [isLoading, setIsLoadind] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch("https://634feb7878563c1d82b38ce5.mockapi.io/items")
+      .then((res) => res.json())
+      .then((json) => {
+        setPizzas(json);
+        setIsLoadind(false);
+      });
+  }, []);
+
+  console.log(pizzas);
+
   return (
     <div className="wrapper">
       <Header />
@@ -17,7 +32,9 @@ export function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock />
+            {isLoading
+              ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+              : pizzas.map((items) => <PizzaBlock key={items.id} {...items} />)}
           </div>
         </div>
       </div>
